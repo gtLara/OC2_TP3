95c4e73237e4c2667713a60ca3e33295886211c6
Lara <gtlchaves@gmail.com>
2022-02-02 08:09:39 -0300

    Remove TODO

--- classes/utils.py
+++ classes/utils.py
@@ -5,7 +5,7 @@ def create_storage(n_items, initial_data="0"):
     data = {}
     n_index_bits = int(log(n_items, 2))
 
-    for item_index in range(n_items): # TODO: check
+    for item_index in range(n_items):
         binary_string_index = "{0:#b}".format(item_index).replace("0b", "")
         padding = "0" * (n_index_bits - len(binary_string_index))
         binary_string_index = padding + binary_string_index
