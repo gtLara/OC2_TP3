846e931bf41b24a83eea223e2d8663248d79aa60
Lara <gtlchaves@gmail.com>
2022-02-02 11:26:33 -0300

    Implementa leitura de memoria em blocos

--- classes/memory.py
+++ classes/memory.py
@@ -1,10 +1,11 @@
-from classes.utils import create_storage, create_random_word
+from classes.utils import create_storage, create_random_word, int_2_padded_bin
 from math import log
 
 class PhysicalMemory:
 
     def __init__(self, n_words=1024, word_size=32, random=True):
 
+        self.n_words = n_words
         self.address_size = int(log(n_words, 2))
 
         if random:
@@ -13,10 +14,23 @@ class PhysicalMemory:
         else:
             self.data = create_storage(n_words)
 
-    def read(self, address):
+    def read(self, address, spread=4): # TODO: review spread policy
 
-        word = self.data[address]
-        return word
+        assert len(address) == self.address_size
+
+        words = create_storage(spread)
+
+        for k, key in enumerate(words.keys()):
+
+            word_address = int(address, 2) + k
+
+            assert word_address < self.n_words
+
+            word_address = int_2_padded_bin(word_address, self.address_size)
+
+            words[key] = self.data[word_address]
+
+        return words
 
     def write(self, address, data):
 
