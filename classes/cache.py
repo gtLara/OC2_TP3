d4a176fe05aa526fc15ab9bc82d55f383ce2be0c
Lara <gtlchaves@gmail.com>
2022-02-03 15:41:56 -0300

    Implementa escrita incompleta e tira byte offset

--- classes/cache.py
+++ classes/cache.py
@@ -19,15 +19,11 @@ class Cache:
                                       self.n_set_bits,
                                       block_size)
 
-
     def decompose_address(self, cpu_address):
         """Decompõe endereços de CPU em campos relevantes."""
 
-        byte_offset = cpu_address[-2:]
-
-        bo_lower = -2
-        bo_upper = -(self.n_block_offset_bits+2)
-        block_offset = cpu_address[bo_upper:bo_lower]
+        bo_upper = -(self.n_block_offset_bits)
+        block_offset = cpu_address[bo_upper:]
 
         index_lower = bo_upper
         index_upper = bo_upper - self.n_set_bits
@@ -38,11 +34,11 @@ class Cache:
 
         tag = cpu_address[:tag_lower] #TODO: understand whats up here
 
-        return byte_offset, block_offset, index, tag
+        return block_offset, index, tag
 
     def read(self, cpu_address, memory, verb=True): # TODO: check
 
-        byte_offset, block_offset, index, tag = self.decompose_address(cpu_address)
+        block_offset, index, tag = self.decompose_address(cpu_address)
         entry = self.entries[index]
 
         if entry.valid_bit and entry.tag == tag: # hit
@@ -66,8 +62,32 @@ class Cache:
 
             return block
 
-    def write(self, cpu_address, data):
-        pass
+    def write(self, cpu_address, data, memory, verb=True):
+
+        block_offset, index, tag = self.decompose_address(cpu_address)
+        print(index)
+        entry = self.entries[index]
+        memory_address = cpu_address[-(memory.address_size):]
+
+        if entry.valid_bit and entry.tag == tag: # hit
+            if verb:
+                print("cache write HIT")
+
+            if entry.dirty_bit == 0 or entry.dirty_bit == 1: #write thru
+
+                memory.write(memory_address, data)
+                self.entries[index].valid_bit = 1
+                self.entries[index].dirty_bit = 0
+                self.entries[index].block[block_offset] = data
+
+        else:
+            if verb:
+                print("cache read MISS")
+
+                memory.write(memory_address, data)
+                self.entries[index].valid_bit = 1
+                self.entries[index].dirty_bit = 0
+                self.entries[index].block[block_offset] = data
 
     def __str__(self):
 
