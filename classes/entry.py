a4bbfcf8b2c47e40e394e1d1fa88c6955ea9ea2b
Lara <gtlchaves@gmail.com>
2022-02-03 13:57:57 -0300

    Acerta passagem de indice

--- classes/entry.py
+++ classes/entry.py
@@ -9,14 +9,14 @@ from math import log
 
 class Entry:
 
-    def __init__(self, n_index_bits=6, block_size=4, random=True):
+    def __init__(self, n_index_bits=6, block_size=4, index=0, random=True):
 
         self.n_index_bits = n_index_bits
         self.block_size = block_size
+        self.index = index
 
         self.dirty_bit = 0
         self.valid_bit = 0
-        self.index = 0
         self.block = {}
         self.tag = 0
 
@@ -32,4 +32,4 @@ class Entry:
         for key, value in self.block.items():
             hex_block[key] = hex(int(value, 2))
 
-        return f"{self.index} | {self.valid_bit} {self.dirty_bit} {self.tag} {hex_block}"
+        return f" {self.index} | {self.valid_bit} | {self.dirty_bit} | {self.tag} | {hex_block}"
