87558aeb0bbe89501624583b1d615ef7a771f27e
Lara <gtlchaves@gmail.com>
2022-02-05 11:52:18 -0300

    Implementa sobrecarga [] para memoria

--- classes/memory.py
+++ classes/memory.py
@@ -1,5 +1,6 @@
-from classes.utils import create_storage, create_random_word, int_2_padded_bin
 from math import log
+import copy
+from classes.utils import create_storage, create_random_word, int_2_padded_bin
 
 class PhysicalMemory:
 
@@ -14,13 +15,13 @@ class PhysicalMemory:
         else:
             self.data = create_storage(n_words)
 
-    def read(self, address, spread=4): # TODO: review spread policy
+    def read_block(self, address, block_size=4): # TODO: review spread policy
 
         assert len(address) == self.address_size
 
-        words = create_storage(spread)
+        block = create_storage(block_size)
 
-        for k, key in enumerate(words.keys()):
+        for k, key in enumerate(block.keys()):
 
             word_address = int(address, 2) + k
 
@@ -28,13 +29,21 @@ class PhysicalMemory:
 
             word_address = int_2_padded_bin(word_address, self.address_size)
 
-            words[key] = self.data[word_address]
+            block[key] = self.data[word_address]
 
-        return words
+        return block
 
     def write(self, address, data):
 
         self.data[address] = data
+
+    def write_block(self, index, tag, entry):
+
+        for block_off, word in entry.block.items():
+            mem_address = (tag + index + block_off)
+            mem_address = mem_address[-(self.address_size):]
+
+            self.write(mem_address, word)
 
     def __str__(self):
 
@@ -44,3 +53,10 @@ class PhysicalMemory:
             mem_viz += f"{k} | {hex(int(v, 2))}\n"
 
         return mem_viz
+
+    def __getitem__(self, slice_key):
+
+        sliced_mem = copy.deepcopy(self)
+        new_data = dict(list(self.data.items())[slice_key])
+        sliced_mem.data = new_data
+        return sliced_mem
