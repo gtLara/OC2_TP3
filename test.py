30a2836f2f1ea00cd5f012530ce989daf3b766e4
Lara <gtlchaves@gmail.com>
2022-02-06 12:29:49 -0300

    Adiciona versao final de output

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 from classes.system import MemorySystem
 
 try:
@@ -14,7 +15,8 @@ with open(f"{fn}", "r") as input_file:
     lines = input_file.readlines()
 
 output_file = open(f"{fn}_output", "w+")
-output_file.write("\n"*7)
+output_log = []
+output_lines = ""
 
 for line in lines:
 
@@ -27,7 +29,8 @@ for line in lines:
 
         memory_system.write(address, data)
 
-        output_file.write(line.strip()+" W\n")
+        output_log.append("WRITES")
+        output_lines += line.strip()+" W\n"
         print(f"mem[{address}] = {hex(int(data, 2))}")
 
     else: # read case
@@ -36,7 +39,26 @@ for line in lines:
 
         word, status = memory_system.read(address)
 
-        output_file.write(line.strip()+f" {status}\n")
+        output_log.append("READS")
+
+        if status == "H":
+            output_log.append("HITS")
+        else:
+            output_log.append("MISSES")
+
+        output_lines += line.strip()+f" {status}\n"
         print(f"value in mem[{address}] is {hex(int(word, 2))}")
+
+output_log = Counter(output_log)
+
+for observation, count in output_log.items():
+    output_file.write(f"{observation}:{count}\n")
+
+hit_rate = output_log["HITS"]/output_log["READS"]
+miss_rate = output_log["MISSES"]/output_log["READS"]
+
+output_file.write(f"HIT RATE:{hit_rate}\n")
+output_file.write(f"MISS RATE:{miss_rate}\n\n")
+output_file.write(output_lines)
 
 output_file.close()
